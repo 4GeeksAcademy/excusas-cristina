88d75c7770f0c4f16762d1d829c1ae1c7b128d6e
Cristina <83121356+cristina1810@users.noreply.github.com>
2025-04-27 09:30:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+console.log("Hola");
+let quien=who[Math.floor(Math.random()*who.length)];
+let accion= action[Math.floor(Math.random()*action.length)];
+let que= what[Math.floor(Math.random()*what.length)];
+let cuando= when[Math.floor(Math.random()*when.length)];
+document.body.innerHTML= quien+" " + accion+" " + que +" "+ cuando;
 };
